853c5ca60ec62dd6923b255d2eb8b3767e55a819
zihadc81 <zihadc81@gmail.com>
2024-07-03 07:10:51 +0600

    Update autoreact.js

--- scripts/commands/autoreact.js
+++ scripts/commands/autoreact.js
@@ -1,67 +1,21 @@
-const fs = require('fs-extra');
-const pathFile = __dirname + '/autoseen/autoreact.txt';
-
-module.exports = {
-config: {
-  name: "autoreact",
-  version: "1.0.0",
+module.exports.config = {
+  name: "aotoreact",
+  version: "2.0.0",
   permission: 0,
-  credits: "nayan",
-  description: "",
-  prefix: 'awto', 
-  category: "auto", 
-  usages: "[off]/[on]",
+  credits: "Nayan",
+  description: "..",
+  prefix: false,
+  category: "...",
+  usages: "",
   cooldowns: 5,
-  dependencies: {
-    "request": "",
-    "fs-extra": "",
-    "axios": ""
-  }
-},
+};
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+ let haha = event.body ? event.body.toLowerCase() : '';
+if (haha.includes(" ") || haha.includes("")){
+const dipto = ['😆','🐸','🙃','😈','🤖','🙄','🙈','🍎','🐰','🎀','🧐','😵','🙂','🤐','💗','😘','😻','😹','😸','💦','🤨','😭','😁','🤣','🤫','😶','🥱','😤','🥵','😇','💋','🙊','🙀','🫶🏻','💛','🖤','🤎','💙','💜','😬','🙆','😏','🌸','🏵️','🍁','🌼','🔥','🐍','👄','✈️','🦛','🦐','🐇','🐮','🐰','🦃','💋','🦋','🍒','🍓','🐼','🍊','🫤','🍍','🍌','🌚','🥥','🫗','🥕','😳','👻','😾','🧀','😒','🥹','☠️','👊','😴','😦','😷','🫣','🫂','🤕','😵','🫢','🤭','😔','💩','💣','👀','🌝','🍼','🐤','😋','😻','😕','🙀']
 
-  languages: {
-  "vi": {},
-      "en": {
-          "off": 'the autoreact function has been disabled for new messages.',
-          "on": 'the autoreact function is now enabled for new messages.',
-        "error": 'incorrect syntax'
-      }
-  },
-
-handleEvent: async ({ api, event, Threads }) => {
-
-  if (!fs.existsSync(pathFile))
-   fs.writeFileSync(pathFile, 'false');
-   const isEnable = fs.readFileSync(pathFile, 'utf-8');
-   if (isEnable == 'true') {
-
-  const reactions = ["🌸","🎉","😫","😻","🙈","💗","🌷","🤭","🥺","😶","👿","🤓"🫦","😾","🤪","🤬","🤫","😼","🪷","😎","🤦","👀","😆","🤡","🤧","😫","😇","😹","😭"];
-  var nayan = reactions[Math.floor(Math.random() * reactions.length)];
-
-  api.setMessageReaction(nayan, event.messageID, (err) => {
-    if (err) {
-      console.error("Error sending reaction:", err);
-    }
-  }, true);
+const r = dipto[Math.floor(Math.random() * dipto.length)];
+return api.setMessageReaction(r, event.messageID, (err) => {}, true)
 }
-},
-
-start: async ({ nayan, events, args, lang }) => {
-   try {
-
-     const logger = require("../../Nayan/catalogs/Nayanc.js");
-     if (args[0] == 'on') {
-       fs.writeFileSync(pathFile, 'true');
-       nayan.sendMessage(lang("on"), events.threadID, events.messageID);
-     } else if (args[0] == 'off') {
-       fs.writeFileSync(pathFile, 'false');
-       nayan.sendMessage(lang("off"), events.threadID, events.messageID);
-     } else {
-       nayan.sendMessage(lang("error"), events.threadID, events.messageID);
-     }
-   }
-   catch(e) {
-     logger("unexpected error while using autoseen function", "system");
-   }
-}
-}
+    };
+module.exports.run = function (){}
